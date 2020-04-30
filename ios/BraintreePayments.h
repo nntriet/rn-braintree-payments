ee59f4c1e764c81fbda3ed5c203d8b46e4a22e67
Nguyen Nhat Triet <triet@corsivalab.space>
2020-04-30 19:50:05 +0700

    [1.0.0] Fix iOS module

--- ios/BraintreePayments.h
+++ ios/BraintreePayments.h
@@ -20,7 +20,7 @@
 - (BOOL)setup:(NSDictionary *)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
 - (NSMutableDictionary *)getPaymentNonce:(NSString* _Nonnull)nonce type:(NSString* _Nonnull)type lastDigits:(NSString* _Nullable)lastDigits isDefault:(BOOL)isDefault;
 - (void)startPayPal:(BTPayPalRequest *)paypalRequest resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
-- (void)startPayPalCheckout:(BTPayPalRequest *)paypalRequest resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject
-- (void)setupPaymentFlowDriver
+- (void)startPayPalCheckout:(BTPayPalRequest *)paypalRequest resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
+- (void)setupPaymentFlowDriver;
 
 @end
